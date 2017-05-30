6809a9da08ab898fb0f502594044c0c0dab90378
Mike Park <Mike.Park@NASA.gov>
2017-05-30 09:02:57 -0400

    better-names

--- hemisphere-cylinder/geometry/hemisph-cyl.sh
+++ hemisphere-cylinder/geometry/hemisph-cyl.sh
@@ -1,8 +1,8 @@
 
 
-~/esp/EngSketchPad/bin/serveCSM -batch hemi-cyl.csm
+~/esp/EngSketchPad/bin/serveCSM -batch hemisph-cyl.csm
 
-~/refine/egads/two/ref_geom_test hsc01.egads hsc01.meshb
+~/refine/egads/two/ref_geom_test hemisph-cyl.egads hsc01.meshb
 
 mv ref_geom_test.gas hsc01.gas
 
