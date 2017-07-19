f906cc9d343441df304964268afa223a93f3c2e2
Mike Park <Mike.Park@NASA.gov>
2017-07-19 09:10:28 -0400

    to include finer start and curvature adaptation

--- hemisphere-cylinder/geometry/hemisph-cyl.sh
+++ hemisphere-cylinder/geometry/hemisph-cyl.sh
@@ -1,8 +1,11 @@
 
-
 ~/esp/EngSketchPad/bin/serveCSM -batch hemisph-cyl.csm
 
-~/refine/egads/two/ref_geom_test hemisph-cyl.egads hsc01.meshb
+~/refine/egads/two/ref_geom_test hemisph-cyl.egads hemisph-cyl.meshb -0.02
 
-mv ref_geom_test.gas hsc01.gas
+mv ref_geom_test.gas hemisph-cyl.gas
+
+~/refine/egads/two/ref_driver -i hemisph-cyl.meshb -g hemisph-cyl.egads -p hemisph-cyl.gas -o hsc01
+
+~/refine/egads/two/ref_translate hsc01.b8.ugrid hsc01.meshb
 
