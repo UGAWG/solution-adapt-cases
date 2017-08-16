21e95c221c95720854add68674abda1982fa8fc1
Mike Park <Mike.Park@NASA.Gov>
2017-08-16 09:35:47 -0400

    run full

--- hemisphere-cylinder/geometry/hemisph-cyl.sh
+++ hemisphere-cylinder/geometry/hemisph-cyl.sh
@@ -3,9 +3,5 @@
 
 ~/refine/egads/two/ref_geom_test hemisph-cyl.egads hemisph-cyl.meshb -0.02
 
-exit
-
-mv ref_geom_test.gas hemisph-cyl.gas
-
 ~/refine/egads/two/ref_driver -i hemisph-cyl.meshb -g hemisph-cyl.egads -o hsc01
 
